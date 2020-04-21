705683f2d3ea56a9bbdb54e3ab653a509f614990
BuildTools <unconfigured@null.spigotmc.org>
2020-04-21 17:20:23 -0400

    Initial commit

---
+++ src/main/java/io/github/lukeeff/forgottenenchantments/ForgottenEnchantments.java
@@ -0,0 +1,20 @@
+package io.github.lukeeff.forgottenenchantments;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ForgottenEnchantments extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        //hi its me darkenedfusion :)
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
